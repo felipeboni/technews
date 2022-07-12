c351003b0be82a1e23dc2832755cb12681d052c8
Felipe Boni <felipeboni@4x4brasil.com.br>
2022-07-12 09:55:32 -0300

    initial commit

---
+++ src/components/Header/Index.tsx
@@ -0,0 +1,18 @@
+import { SignInButton } from '../SignInButton/index';
+import styles from './styles.module.scss'
+
+export function Header() {
+    return (
+        <header className={styles.headerContainer}>
+            <div className={styles.headerContent}>
+                <img src="/images/logo.svg" alt="technews logo" />
+                <nav>
+                    <a className={styles.active}>Home</a>
+                    <a>Posts</a>
+                </nav>
+
+                <SignInButton />
+            </div>
+        </header>
+    );
+}
---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,34 @@
+import { FaGithub } from 'react-icons/fa'
+import { FiX } from 'react-icons/fi'
+import { signIn, signOut, useSession } from 'next-auth/react'
+
+import styles from './styles.module.scss'
+
+
+export function SignInButton() {
+    const { data: session } = useSession()
+
+    return session ? (
+        <button
+            className={styles.btnSignIn}
+            type="button"
+            onClick={() => signOut('github')}
+        >
+                <FaGithub color="#52AACA"/>
+                {session.user.name}
+                <FiX
+                color="#7C8D88"
+                className={styles.closeIcon}
+            />
+        </button>
+    ) : (
+        <button
+            className={styles.btnSignIn}
+            type="button"
+            onClick={() => signIn('github')}
+        >
+                <FaGithub color="#00B188"/>
+                Sign In with Github
+        </button>
+    );
+}
